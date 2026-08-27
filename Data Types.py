cbcc8a42abc029b2eec280d5653227ba959390bf
Will <billbillster7@gmail.com>
2026-08-27 13:55:10 -0500

    Add Data Types assignment with variable definitions and print statements

---
+++ Data Types.py
@@ -0,0 +1,29 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+"""
+   Assignment 2B: Data Types
+   8/27/2026
+   Data Types
+"""
+
+player_class = "Worrior"
+player_level = 21
+player_critical_hit_percentage = 2.3
+player_is_infected = False
+
+print(f"Character Class: {player_class}")
+print(f"Character Level: {player_level}")
+print(f"Character Critical Hit: {player_critical_hit_percentage}%")
+print(f"Infected: {player_is_infected}")
