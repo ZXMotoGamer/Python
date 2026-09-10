5f66f0e9dba04f1e094d31b63164764c7255e1b5
Will <billbillster7@gmail.com>
2026-09-10 13:54:16 -0500

    Comment out price calculation and age-related logic for future reference

--- Programming Logic Course/Test $.py
+++ Programming Logic Course/Test $.py
@@ -1,35 +1,52 @@
-price = float(input("cost of pie? "))
-print(f"pie is {f'${price:.2f}':>21}")
-# print(f"pie is ${price:>20.2f}")
-# print(f"pie is                                   ${price:.2f}")
-# print(f"pie is ................................. ${price:.2f}")
-# can the $ symbol be attached to the price variable so that is is printed in front of it without using spaces or dots.
+# price = float(input("cost of pie? "))
+# print(f"pie is {f'${price:.2f}':>21}")
+# # print(f"pie is ${price:>20.2f}")
+# # print(f"pie is                                   ${price:.2f}")
+# # print(f"pie is ................................. ${price:.2f}")
+# # can the $ symbol be attached to the price variable so that is is printed in front of it without using spaces or dots.
 
 
-user_day = input("\nWhat day is it? ").lower()
+# user_day = input("\nWhat day is it? ").lower()
 
-match user_day:
-    case "tuesday":
-        child_price_per_year = 0.50
-    case "sunday":
-        child_price_per_year = 1.00
-        print("\nFree Drinks!")
-    case _:
-        child_price_per_year = 1.00
+# match user_day:
+#     case "tuesday":
+#         child_price_per_year = 0.50
+#     case "sunday":
+#         child_price_per_year = 1.00
+#         print("\nFree Drinks!")
+#     case _:
+#         child_price_per_year = 1.00
 
-age = int(input("\nWhat is you're age? "))
+# age = int(input("\nWhat is you're age? "))
 
 
-if age < 1:
-    print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:$0.00 Free of Charge!\n")
-elif age < 13:
-    print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:${child_price_per_year * age:.2f}\n")
-elif age < 65:
-    print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:$16.95\n")
-elif age > 64:
-    print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:$12.95\n")
+# if age < 1:
+#     print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:$0.00 Free of Charge!\n")
+# elif age < 13:
+#     print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:${child_price_per_year * age:.2f}\n")
+# elif age < 65:
+#     print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:$16.95\n")
+# elif age > 64:
+#     print(f"\nDay:{user_day}\t\tAge:{age}\t\tPrice:$12.95\n")
 
 
 # how do I have "Free Drinks" print after the (if/elif) function but also attached to only appear for the (match) day of sunday?
 # is this even necessary?
 # should I care?
+
+
+board = True
+
+while board:
+    print("\nCan we sing a song?")
+
+    answer = input("\nParents say: ").lower()
+
+    if answer == "yes":
+        board = print(
+            "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall."
+        )
+        for n, m in zip(range(98, 0, -1), range(97, 0, -1)):
+            print(
+                f"{n} bottles of beer on the wall, {n} bottles of beer. Take one down and pass it around, {m} bottles of beer on the wall."
+            )
